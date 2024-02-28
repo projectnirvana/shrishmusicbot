c6fbcd41ce4df2257e9f4a0300f442a7fe7a28bb
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-02-28 21:48:08 +0530

    Update gitinfo.py

--- MOONMUSIC/plugins/tools/gitinfo.py
+++ MOONMUSIC/plugins/tools/gitinfo.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 @app.on_message(filters.command(["github", "git"]))
 async def github(_, message):
     if len(message.command) != 2:
-        await message.reply_text("/git Moonshining6")
+        await message.reply_text("/git Moonshining1")
         return
 
     username = message.text.split(None, 1)[1]
